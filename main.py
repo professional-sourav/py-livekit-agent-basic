6926fc0bf79bc1fa2770572b825421c00792dc8a
Sourav Chakraborty <professional.sourav@gmail.com>
2025-09-02 00:22:21 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from livekit.agents import (
+    Agent,
+    AgentSession,
+    JobContext,
+    RunContext,
+    WorkerOptions,
+    cli,
+    function_tool,
+)
+from livekit.plugins import openai, silero
+from dotenv import load_dotenv
+
+load_dotenv()
+
+@function_tool
+async def lookup_weather(
+    context: RunContext,
+    location: str,
+):
+    """Used to look up weather information."""
+
+    return {"weather": "sunny", "temperature": 70}
+
+
+async def entrypoint(ctx: JobContext):
+    await ctx.connect()
+
+    agent = Agent(
+        instructions="You are a friendly voice assistant built by LiveKit.",
+        tools=[lookup_weather],
+    )
+    session = AgentSession(
+        vad=silero.VAD.load(),
+        # any combination of STT, LLM, TTS, or realtime API can be used
+        stt=openai.STT(model="whisper-1"),
+        llm=openai.LLM(model="gpt-4o-mini"),
+        tts=openai.TTS(),
+    )
+
+    await session.start(agent=agent, room=ctx.room)
+    await session.generate_reply(instructions="greet the user and ask about their day")
+
+
+if __name__ == "__main__":
+    cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint))
